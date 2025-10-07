84280ee21c0289d434cb5412edd3337e95dc1f74
Joao Pedro Santiago Ferreira <jplok1025@outlook.com>
2025-10-07 11:29:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efeito de rolagem suave nos links (opcional)
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
